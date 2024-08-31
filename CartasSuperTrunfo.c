d7f4b8f9f325452a5487da526f3bc635ef5653a4
JBoryca <julianaboryca@gmail.com>
2024-08-31 01:22:00 +0000

    Primeira versão do projeto Super Trunfo com declaração de variáveis, inserção de dados pelo usuário e visualização do dado inserido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+        // Declara variáveis para armazenar os dados inseridos pelo usuário
+        char carta[3];
+        float populacao;
+        float area;
+        float PIB;
+        int turismo;
+     
+    // Solicita ao usuário que insira as informações para variáveis
+    printf("Super Trunfo - Países\nInsira o código da cidade:\n");
+    scanf("%s", &carta);
+
+    printf("Insira a população:\n");
+    scanf("%f", &populacao);
+
+    printf("Insira a área:\n");
+    scanf("%f", &area);
+
+    printf("Insira o PIB:\n");
+    scanf("%f", &PIB);
+
+    printf("Insira o número de pontos turísticos:\n");
+    scanf("%d", &turismo);
+
+    //Exibe os dados inseridos pelo usuário
+    printf("Super Trunfo\nCógido da cidade: %s \n", carta);
+    printf("População: %.f \n", populacao);
+    printf("Área: %.f \n", area);
+    printf("PIB: %.f\n", PIB);
+    printf("Pontos Turísticos: %d \n", turismo);
+
+    
+   return 0; //Finaliza o programa
 }
