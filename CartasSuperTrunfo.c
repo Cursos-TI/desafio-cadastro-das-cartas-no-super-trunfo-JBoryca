c88194e03689bbce330f66d3fc479ab38d1dee0d
JBoryca <julianaboryca@gmail.com>
2024-09-06 03:02:37 +0000

    Acrescentei a comparação entre as cartas por meio de operadores racionais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,44 +2,39 @@
 
 int main() {
 
-    //Declaração das variáveis
+    //Declaração das variáveis para armazenar os dados
 
-        char estado1;
-        char estado2;
-
-        char carta1[3];
+        char carta1[3]; //Código das cartas
         char carta2[3];
 
-        char cidade1[30];
+        char cidade1[30]; //Nome das cidades
         char cidade2[30];
 
-        unsigned int populacao1;
+        unsigned int populacao1; //População
         unsigned int populacao2;
 
-        float densidade1;
+        float densidade1; //Densidade populacional
         float densidade2;
 
-        float area1;
+        float area1; //Área por km2
         float area2;
 
-        float PIB1;
+        float PIB1; //PIBs
         float PIB2;
 
-        float capita1; 
+        float capita1; //PIB per capita
         float capita2; 
 
-        int turismo1;
+        int turismo1; //Pontos turísticos
         int turismo2;
 
-        int resultado1, resultado2; 
-     
-
-    //Informações para a inserção de dados da carta 1 pelo usuário
+           
+    //Coleta de dados da carta 1 
    
     printf("***** Super Trunfo - Países *****\n");
 
-    printf("** Carta 01 ** \n");
-
+    printf("** Carta 1 ** \n");
+    
     printf("Insira o código da carta (código do Estado de A a H + número da cidade de 01 a 04):\n");
     scanf("%s", &carta1);
 
@@ -58,14 +53,14 @@ int main() {
     printf("Insira o número de pontos turísticos:\n");
     scanf("%d", &turismo1);
 
+    //Cálculo de PIB per capita e densidade populacional
     densidade1 = populacao1/area1;
     capita1 = PIB1/populacao1;
 
-    //Imprimi 
+    //Exibição dos dados da carta 1
   
-    printf("*** Carta 01 inserida com sucesso ***\n");
-    printf("Estado: %c\n", estado1);
-    printf("Cógido da carta: %s \n", carta1);
+    printf("*** Carta 1 inserida com sucesso ***\n");
+    printf("Código da carta: %s\n", carta1);
     printf("Cidade: %s\n", cidade1);
     printf("População: %.2u \n", populacao1);
     printf("Área: %.2f km2\n", area1);
@@ -74,10 +69,9 @@ int main() {
     printf("PIB per capita: %.2f\n", capita1);
     printf("Pontos Turísticos: %d\n", turismo1);
 
-    //Imprimi as informações para a inserção de dados da carta 2 pelo usuário d
+    //Coleta de dados da carta 2 
 
-    printf("** Carta 02 ** \n");
-
+    printf("** Carta 2 ** \n");
 
     printf("Insira o código da carta (código do Estado de A a H + número da cidade de 01 a 04):\n");
     scanf("%s", &carta2);
@@ -97,12 +91,13 @@ int main() {
     printf("Insira o número de pontos turísticos:\n");
     scanf("%d", &turismo2);
 
+    //Cálculo de PIB per capita e densidade populacional
     densidade2 = populacao2/area2;
     capita2 = PIB2/populacao2;
-  
-    printf("*** Carta 02 inserida com sucesso ***\n");
-    printf("Estado: %c\n", estado2);
-    printf("Cógido da carta: %s \n", carta2);
+
+    //Exibição dos dados da carta 2
+    printf("*** Carta 2 inserida com sucesso ***\n");
+    printf("Código da carta: %s\n", carta2);
     printf("Cidade: %s\n", cidade2);
     printf("População: %.2u \n", populacao2);
     printf("Área: %.2f km2\n", area2);
@@ -111,10 +106,61 @@ int main() {
     printf("PIB per capita: %.2f\n", capita2);
     printf("Pontos Turísticos: %d\n", turismo2);
 
-    printf("*** Comparando as cartas inseridas ***");
+    //Inicio da comparação entre as propriedades das cartas
+    printf("*** Comparação entre as cartas ***\n");
+             
+    //Declaração de variáveis para a comparação
+    int totalPopulacao;    
+    int totalArea;   
+    int totalDensidade;
+    int totalPIB;
+    int totalCapita;    
+    int totalTurismo;
+    float totalCarta1; //Total das propriedades da carta 1
+    float totalCarta2; // Total das propriedades da carta 2
+    int totalCartaVencedora; // Determina a carta vencedora pela soma das propriedades
 
-    
+    //Comparação das propriedades utilizando operadores racionais
+    totalPopulacao = (populacao2 > populacao1); //Verifica se a população da carta 2 é maior que a carta 1
+    totalPopulacao = totalPopulacao+1; //Adiciona 1 para indicar qual é a carta vencedora
 
+    printf("Propriedade - População: A carta vencedora é a carta de número: %d \n", totalPopulacao);
+
+    totalArea = (area2 > area1);//Verifica se a área da carta 2 é maior que a carta 1
+    totalArea = totalArea+1; //Adiciona 1 para indicar qual é a carta vencedora
+
+    printf("Propriedade - Área: A carta vencedora é a carta de número: %d \n", totalArea);
+
+    totalDensidade = (densidade2 < densidade1); //Verifica se a área da carta 2 é menor que a carta 1
+    totalDensidade = totalDensidade+1;//Adiciona 1 para indicar qual é a carta vencedora
+
+    printf("Propriedade - Densidade Populacional: A carta vencedora é a carta de número: %d \n", totalDensidade);
+
+    totalPIB = (PIB2 > PIB1);//Verifica se o PIB da carta 2 é maior que a carta 1
+    totalPIB = totalPIB+1;//Adiciona 1 para indicar qual é a carta vencedora
     
+    printf("Propriedade - PIB: A carta vencedora é a carta de número: %d \n", totalPIB);
+
+    totalCapita = (capita2 > capita1); //Verifica se o PIB per capita da carta 2 é maior que a carta 1
+    totalCapita = totalCapita+1;//Adiciona 1 para indicar qual é a carta vencedora
+    
+    printf("Propriedade - PIB per capita: A carta vencedora é a carta de número: %d \n", totalCapita);
+
+    totalTurismo = (turismo2 > turismo1);//Verifica se os pontos turísticos da carta 2 é maior que a carta 1
+    totalTurismo = totalTurismo+1;//Adiciona 1 para indicar qual é a carta vencedora
+
+    printf("Propriedade - Turismo: A carta vencedora é a carta de número: %d \n", totalArea);
+  
+   //Cálculo da soma das propriedades (exceto densidade populacional)
+    totalCarta1 = populacao1 + area1 + PIB1 + capita1 + turismo1; 
+    totalCarta2 = populacao2 + area2 + PIB2 + capita2 + turismo2;
+
+    //Determinação da carta vencedora com base na soma das propriedades
+    totalCartaVencedora = (totalCarta2 > totalCarta1);
+    totalCartaVencedora = totalCartaVencedora+1; // Adiciona 1 para indicar a carta vencedora
+
+    printf("Soma de todas as propriedades: a carta vencedora é a carta de número: ** %d **\n", totalCartaVencedora);
+
+             
    return 0;
 }
